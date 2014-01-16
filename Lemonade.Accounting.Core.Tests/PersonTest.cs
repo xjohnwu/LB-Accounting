198427a3791f2899cf1528d6e9a45ba9891e5f38
xjohnwu <xjohnwu@gmail.com>
2014-01-16 23:14:28 +0000

    tests

--- Lemonade.Accounting.Core.Tests/PersonTest.cs
+++ Lemonade.Accounting.Core.Tests/PersonTest.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Lemonade.Accounting.Core.Tests
 {
     public class PersonTest
     {
+		[Test]
+		public void A_pay_B()
+		{
+			double a = 3;
+			Assert.AreEqual(2, a);
+
+		}
     }
 }
